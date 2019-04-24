33dcbb5f0a887bbfe700298a6e21b51f5c6dd484
ncandaffio <44885428+ncandaffio@users.noreply.github.com>
2019-04-24 18:48:18 -0400

    Table Populated

--- static/js/app.js
+++ static/js/app.js
@@ -2,3 +2,14 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+var tbody = d3.select('#ufo-table').select('tbody')
+console.log(Object.values(tableData[0])[0])
+
+// //Loop and add the rows
+tableData.forEach(sighting => {
+    var row = tbody.append('tr')
+    //Loop and populate cells
+    Object.values(sighting).forEach(value => {
+        row.append('td').text(value)
+    })
+})
