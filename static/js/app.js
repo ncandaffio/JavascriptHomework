607ae857b5d6eff1933ebeb71d1d82496a7204ea
ncandaffio <44885428+ncandaffio@users.noreply.github.com>
2019-04-26 00:32:58 -0400

    part 1 complete

--- static/js/app.js
+++ static/js/app.js
@@ -2,14 +2,40 @@
 var tableData = data;
 
 // YOUR CODE HERE!
-var tbody = d3.select('#ufo-table').select('tbody')
-console.log(Object.values(tableData[0])[0])
+var tbody = d3.select('#ufo-table').select('tbody');
+var button = d3.select("#filter-btn")
 
 // //Loop and add the rows
-tableData.forEach(sighting => {
-    var row = tbody.append('tr')
-    //Loop and populate cells
-    Object.values(sighting).forEach(value => {
-        row.append('td').text(value)
+function populateTable(data){
+    tbody.selectAll("tr").remove();
+    data.forEach(sighting => {
+        var row = tbody.append('tr')
+        //Loop and populate cells
+        Object.values(sighting).forEach(value => {
+            row.append('td').text(value);
+        })
     })
-})
+};
+
+function init(){
+    populateTable(tableData);
+}
+
+function filtertable(event){
+    console.log("filtering...")
+    d3.event.preventDefault();
+    var dateFilter = d3.select('#datetime').node().value;
+    console.log(dateFilter)
+
+    if (dateFilter === ""){populateTable(tableData);}
+
+    else {
+        var filteredData = tableData.filter(sighting => sighting.datetime === dateFilter);
+        console.log(filteredData);
+        populateTable(filteredData);
+    }
+};
+
+button.on("click", filtertable)
+
+init();
